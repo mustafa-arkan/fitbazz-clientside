53fbadf5ce94fba43d9553ee31dc96e6390fbe0f
mustafa-arkan <arkancse@gmail.com>
2022-11-09 18:53:32 +0600

    react photo view done

--- src/Pages/Services/ServiceCard.js
+++ src/Pages/Services/ServiceCard.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import 'react-photo-view/dist/react-photo-view.css';
+import { PhotoProvider, PhotoView } from 'react-photo-view'
+
+
+
 
 const ServiceCard = ({service}) => {
 
@@ -7,7 +12,25 @@ const ServiceCard = ({service}) => {
 
     return (
         <div    className="card card-compact w-25 bg-base-100 shadow-xl d-flex">
-            <figure><img src={img} alt="fitness" className='w-100' /></figure>
+
+
+
+
+
+{/* <figure><img src={img} alt="fitness" className='w-100' /></figure> */}
+
+
+
+ 
+<PhotoProvider>
+      <PhotoView src={img}>
+      <img src={img} alt="fitness" className='w-100' />
+      </PhotoView>
+    </PhotoProvider>
+
+
+
+            
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
                 <p className='text-2xl text-orange-600 font-semibold'>Price: ${price}</p>
